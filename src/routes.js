486bd39627d2b9f06584d1a6f5b5cbc22144b3e2
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-05 00:01:32 +0100

    1v
    Routa dos favoritos finalizada

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,14 @@ routes.post('/addFavoritos', (req, res) =>{
     const corpo = req.body
     const sqlCommand = 'INSERT INTO favoritos SET ?;'
 
-
+    conection.query(sqlCommand, corpo, (err, result) =>{
+        if(err){
+            res.status(400).json({'erro': err})
+        }else{
+            res.status(201).json(result)
+        }
+    })
 })
+
 
 export default routes
