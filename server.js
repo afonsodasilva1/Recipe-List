1fd00d973c2265e4c3245da1b3872913b2418c07
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-04 23:36:29 +0100

    1v
    Servidor finalizado

---
+++ server.js
@@ -0,0 +1,8 @@
+import routes from './src/routes.js';
+
+const PORT = 3003
+
+routes.listen(PORT, () =>{
+
+    console.log(`Servidor rodando no endereço: http://localhost:${PORT}`)
+})
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const routes = express()
+
+routes.get('', (req, res) =>{
+
+})
+
+export default routes
