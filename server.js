544fd786d0039181bb85b24a01a275f76a5ba25a
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2023-07-06 06:59:55 +0100

    v1
    Alterei o endereço no arquivo server

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const PORT = 3010
 
 routes.listen(PORT, () =>{
 
-    console.log(`Servidor rodando no endereço: http://localhost:${PORT}/`)
+    console.log(`Servidor rodando no endereço: http://localhost:${PORT}/listarReceita`)
 })
